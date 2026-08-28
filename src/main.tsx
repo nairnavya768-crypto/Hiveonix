61a35dc60cb06bfdfc770f25d571ddc868cf3c86
nairnavya768-crypto <nairnavya768@gmail.com>
2026-08-28 06:26:45 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+// Application entry point. This starts the React application and loads App.tsx and index.css
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
